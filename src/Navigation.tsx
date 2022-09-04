c016fe52f5b0087b8f41e38b80d40a5ffa7cb949
Patrick Kirk <kirk0216@algonquinlive.com>
2022-09-03 20:45:19 -0400

    Fix: Replace link destinations with '/' (current page) instead of '#' to satisfy jsx-a11y/anchor-is-valid.

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -22,17 +22,17 @@ export class Navigation extends Component {
         return (
             <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
                 <div className='container-fluid'>
-                    <a className='navbar-brand' href='#'>Patrick Kirk</a>
+                    <a className='navbar-brand' href='/'>Patrick Kirk</a>
                     <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbar-items' aria-controls='narbar-items' aria-expanded='false' aria-label='Toggle Navigation'>
                         <span className='navbar-toggler-icon'></span>
                     </button>
 
                     <div className='collapse navbar-collapse' id='navbar-items'>
                         <ul className='navbar-nav'>
-                            <NavigationItem text='Home' url='#' />
-                            <NavigationItem text='Portfolio' url='#' />
-                            <NavigationItem text='Resume' url='#' />
-                            <NavigationItem text='Contact' url='#' />
+                            <NavigationItem text='Home' url='/' />
+                            <NavigationItem text='Portfolio' url='/' />
+                            <NavigationItem text='Resume' url='/' />
+                            <NavigationItem text='Contact' url='/' />
                         </ul>
                     </div>
                 </div>
