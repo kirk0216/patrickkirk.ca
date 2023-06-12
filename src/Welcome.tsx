43501d77969bd206ef77f73ef2f16b0fa2c5b0d6
Patrick Kirk <kirk0216@algonquinlive.com>
2023-06-12 19:20:25 -0400

    Remove link from "contact me" on Welcome.

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -13,7 +13,7 @@ export class Welcome extends React.Component {
                 </p>
 
                 <p>
-                    Thank you for taking the time to visit. If you have any questions, please feel free to <a href='#contact'>contact me</a>.
+                    Thank you for taking the time to visit. If you have any questions, please feel free to contact me.
                 </p>
             </div>
         )
